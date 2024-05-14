f27e12a99a401cbf8642d5e4c7821d69c6028bc6
Costantino14 <costantino.grabesu14@gmail.com>
2024-05-15 00:16:01 +0200

    modifiche index, carosello

--- index/script.js
+++ index/script.js
@@ -1,9 +1,50 @@
-const url = `https://striveschool-api.herokuapp.com/api/deezer/search?q=queen`
+const url = `https://striveschool-api.herokuapp.com/api/deezer/search?q=`
+const contenuto = document.getElementById("carouselExampleSlidesOnly")
 
-fetch(url)
+const artisti= ["eminem", "salmo", "metallica", "lazza", "oasis"]
+const index= Math.floor(Math.random() * (artisti.length - 0));
+
+fetch(url+artisti[index])
   .then(response => response.json())  // Converte la risposta in JSON
-  .then(data => console.log(data))    // Stampa i dati nella console
+  .then(braniOttenuti => {let canzoni = braniOttenuti.data
+    let i=0;
+    let active;
+    canzoni.forEach(canzone => {
+      i++;
+     
+      let card = document.createElement("div");
+      card.classList.add("carousel-item");
+      if(i===1) {
+        card.classList.add("active");
+      }
+      card.innerHTML = `
+      <div class="d-flex">
+          <img src="${canzone.album.cover}" alt="${canzone.title}" style="width: 200px;">
+        <div class="card-body">
+          <p>Album</p>
+          <h1>${canzone.title}</h1>
+          <p>${canzone.artist.name}</p>
+          <p>Ascolta il nuovo singolo di ${canzone.artist.name}</p>
+          <div>
+            <button type="button" class="btn btn-success">Play</button>
+            <button type="button" class="btn btn-dark">Salva</button>
+            <i class="bi bi-three-dots"></i>
+          </div>
+        </div>
+      </div>
+        `;
+      contenuto.appendChild(card);
+
+    });
+  }
+    
+
+
+    )    // Stampa i dati nella console
   .catch(error => console.error('Errore nella fetch:', error)); // Gestisce eventuali errori
+
+
+
 
 
 
