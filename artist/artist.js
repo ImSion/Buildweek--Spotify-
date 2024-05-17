e18530c3be572ee00d083f48615bb847bf10d859
gg110400 <giuliagiudici120@gmail.com>
2024-05-17 12:32:37 +0200

    sidebar a scomparsa

--- artist/artist.js
+++ artist/artist.js
@@ -48,7 +48,7 @@ document.addEventListener("DOMContentLoaded", function () {
         const formattedDuration = duration.replace(/\./g, ":");
         // Assegna la durata formattata all'elemento .duration
         durations[index].innerText = formattedDuration;
-        albumCovers[index].src=dati.album.cover_small;
+        albumCovers[index].src=dati.album.cover_medium;
         fotoBand.src = dati.album.cover_small;  
         bandName.innerText = "Di " + dati.artist.name;
         artist.innerText=dati.artist.name;
@@ -69,3 +69,65 @@ document.addEventListener("DOMContentLoaded", function () {
       console.error("There was a problem with your fetch operation:", error);
     });
 });
+
+
+// funzione per far sparire la sidebar di destra con scritto attività amici
+
+ function closeSideBar () {
+
+  //punto il pulsante con la x
+  let closeButton=document.getElementById("close-button");
+
+  //punto la sidebar di destra
+
+  let sideBarDestra=document.getElementById("sb-right");
+
+  //punto il div centrale perchè voglio che si espanda quando la barra scompare
+  let divCentrale=document.querySelector(".central-div");
+
+  //punto il div in cui si trova il pulsante
+    let openDiv=document.getElementById("open-sidebar");
+
+     //punto il pulsantino per aprire la sidebar
+  let openButton=document.getElementById("open-button");
+
+  // aggiungo un event listener al pulsante x, quando premo su x la sidebar scompare
+
+   closeButton.addEventListener("click", function() {
+    sideBarDestra.style.display="none";
+    sideBarDestra.style.width="0px";
+    sideBarDestra.classList.remove("p-3")
+    divCentrale.style.flexGrow = "1";
+
+      if ((sideBarDestra.style.display === "none") && sideBarDestra.style.width==="0px") {
+          openDiv.style.height = "100vh";
+          openDiv.style.width = "100px";
+          openDiv.style.display = "flex";
+          openButton.style.display = "inline";
+      }
+
+});
+
+}
+
+
+function openSideBar () {
+
+  //punto la sidebar di destra
+  let sideBarDestra=document.getElementById("sb-right");
+
+ //punto il div in cui si trova il pulsante
+ let openDiv=document.getElementById("open-sidebar");
+
+  //punto il pulsantino per aprire la sidebar
+  let openButton=document.getElementById("open-button");
+  openButton.addEventListener("click", function () {
+    sideBarDestra.classList.add("d-flex", "flex-column", "flex-shrink-0", "p-3");
+    openButton.style.display="none";
+    openDiv.style.width="0px";
+    sideBarDestra.style.width="320px";
+
+  })
+
+
+}
